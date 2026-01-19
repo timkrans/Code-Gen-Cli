ea7b2a957eaccbf21859e8ce6869a7adce46e9da
timkrans <tkransbe@asu.edu>
2026-01-18 22:31:33 -0500

    Getting the ask feature it up to date and removing a usesless return

--- agents/ask/llm.go
+++ agents/ask/llm.go
@@ -11,7 +11,7 @@ import(
 	"io"
 )
 
-func GenerateAnswer(prompt string)(string, error){
+func GenerateAnswer(prompt string)(error){
 	reqBody := models.OllamaRequest{
         Model:  "llama3.2",
         Prompt: prompt,
@@ -20,17 +20,17 @@ func GenerateAnswer(prompt string)(string, error){
 
     reqBytes, err := json.Marshal(reqBody)
     if err != nil {
-        return "", err
+        return err
     }
 
     resp, err := http.Post("http://localhost:11434/api/generate", "application/json", bytes.NewBuffer(reqBytes))
     if err != nil {
-        return "", fmt.Errorf("failed to connect to Ollama: %w", err)
+        return fmt.Errorf("failed to connect to Ollama: %w", err)
     }
     defer resp.Body.Close()
 
     if resp.StatusCode != http.StatusOK {
-        return "", fmt.Errorf("Ollama returned non-200: %d", resp.StatusCode)
+        return fmt.Errorf("Ollama returned non-200: %d", resp.StatusCode)
     }
 
 	var fullResponse strings.Builder
@@ -43,11 +43,11 @@ func GenerateAnswer(prompt string)(string, error){
             if errors.Is(err, io.EOF) {
                 break
             }
-            return "", fmt.Errorf("failed to decode Ollama stream: %w", err)
+            return fmt.Errorf("failed to decode Ollama stream: %w", err)
         }
 
         fullResponse.WriteString(chunk.Response)
     }
-	//fmt.Printf(fullResponse.String())
-	return fullResponse.String(), nil
+	fmt.Printf(fullResponse.String())
+	return nil
 }
