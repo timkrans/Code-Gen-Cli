7b77da47180ab53dc6684704fa2d73bff33e9d38
timkrans <tkransberger@yahoo.com>
2026-01-19 09:19:20 -0500

    Adding the models to the main so ask can fully work

---
+++ agents/models/models.go
@@ -0,0 +1,12 @@
+package models
+
+type OllamaRequest struct {
+    Model  string `json:"model"`
+    Prompt string `json:"prompt"`
+    Stream bool   `json:"stream"`
+}
+
+type OllamaResponse struct {
+    Response string `json:"response"`
+    Done     bool   `json:"done"`
+}
