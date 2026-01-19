fe7c9a7fd515af97cd109a91b091daea14d9fe76
timkrans <tkransbe@asu.edu>
2026-01-18 22:25:39 -0500

    Added the module for asking a basic question TODO add it as an option in the main loop

---
+++ agents/ask/llm.go
@@ -0,0 +1,53 @@
+package ask
+
+import(
+	"encoding/json"
+	"strings"
+	"code-gen-cli/agents/models"
+	"fmt"
+	"errors"
+	"net/http"
+	"bytes"
+	"io"
+)
+
+func GenerateAnswer(prompt string)(string, error){
+	reqBody := models.OllamaRequest{
+        Model:  "llama3.2",
+        Prompt: prompt,
+        Stream: true,
+    }
+
+    reqBytes, err := json.Marshal(reqBody)
+    if err != nil {
+        return "", err
+    }
+
+    resp, err := http.Post("http://localhost:11434/api/generate", "application/json", bytes.NewBuffer(reqBytes))
+    if err != nil {
+        return "", fmt.Errorf("failed to connect to Ollama: %w", err)
+    }
+    defer resp.Body.Close()
+
+    if resp.StatusCode != http.StatusOK {
+        return "", fmt.Errorf("Ollama returned non-200: %d", resp.StatusCode)
+    }
+
+	var fullResponse strings.Builder
+    decoder := json.NewDecoder(resp.Body)
+
+    for decoder.More() {
+        var chunk models.OllamaResponse
+        err := decoder.Decode(&chunk)
+        if err != nil {
+            if errors.Is(err, io.EOF) {
+                break
+            }
+            return "", fmt.Errorf("failed to decode Ollama stream: %w", err)
+        }
+
+        fullResponse.WriteString(chunk.Response)
+    }
+	//fmt.Printf(fullResponse.String())
+	return fullResponse.String(), nil
+}
